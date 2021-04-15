6e88402ed67d398e3a57685d20711d90b2b6715a
Yoshiki <yosyos0306@gmail.com>
2021-04-15 18:37:36 +0900

    syntax highlight の行数を調整

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
+/* Global CSS */
 import "./src/App.css"
 
+/* Syntax Highligt 用 */
 require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
