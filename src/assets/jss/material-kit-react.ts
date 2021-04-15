f40d52b55ceba1ef3701550bfabbf065c3bfbc8e
Yoshiki <yosyos0306@gmail.com>
2021-04-15 21:02:47 +0900

    カラーパレット調整

--- src/assets/jss/material-kit-react.ts
+++ src/assets/jss/material-kit-react.ts
@@ -71,7 +71,7 @@ const defaultFont: CSSProperties = {
   lineHeight: "1.5em",
 }
 
-const primaryColor = "#9c27b0"
+const primaryColor = "#496B73"
 const warningColor = "#ff9800"
 const dangerColor = "#f44336"
 const successColor = "#4caf50"
