cd2e06d79abb9c00ac5f51cf02886d52bb0c2133
yosyos36 <yosyos0306@gmail.com>
2021-06-19 12:44:18 +0900

    clean: remove debug console.log

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,6 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
-    console.log(slug)
     createNodeField({
       node,
       name: `slug`,
